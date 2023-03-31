71ae431bfb015ee4e4b02ac3f30b79a337ace285
mony0112 <113718634+mony0112@users.noreply.github.com>
2023-03-31 12:30:57 -0700

    Update main.py

--- Final Project/main.py
+++ Final Project/main.py
@@ -2,6 +2,9 @@ import js
 p5 = js.window
 
 
+arr = []
+
+
 class Pepper():
     def __init__(self, x = 0, y = 0):
         self.x = x
@@ -13,7 +16,7 @@ class Pepper():
         scaleX = p5.width / self.img.width
         scaleY = p5.height / self.img.height
         p5.scale(scaleX, scaleY)  # Scale the image based on the ratio of canvas size to image size
-        #p5.image(self.img, self.x, self.y)  # Draw the image
+        p5.image(self.img, self.x, self.y)  # Draw the image
         p5.pop()  # Restore the transformation state
         
     def draw_dots(self, num_dots = 10, dot_size = 10):
@@ -26,10 +29,31 @@ class Pepper():
             color = self.img.get(int(x / scaleX), int(y / scaleY))
             p5.fill(color)
             p5.noStroke()
-            p5.ellipse(x, y, dot_size, dot_size)
+            # p5.ellipse(x, y, dot_size, dot_size)
+            arr.append({'x': x, 'y': y, 'color': color})
+
+
+        for item in arr:
+
+
+            if p5.mouseX - 50 < item['x'] < p5.mouseX + 50 and p5.mouseY - 50 < item['y'] < p5.mouseY + 50:
+                continue
+            
+            p5.stroke(5)
+            p5.noFill()
+            p5.rect(p5.mouseX - 50, p5.mouseY - 50, 100, 100)
+
+
+            p5.fill(item['color'])           
+            p5.noStroke()
+            p5.ellipse(item['x'], item['y'], dot_size, dot_size)
+
+
 
 
 pepper = Pepper()
+
+
 
 
 def setup():
@@ -46,11 +70,15 @@ def keyPressed(event):
     pass 
 
 
+
+
 def keyReleased(event):
     pass
     
 def mousePressed(event):
     pass
+
+
 
 
 def mouseReleased(event):
