612c6017f5745b5ceb437908b6e8026112ccb7b4
mony0112 <113718634+mony0112@users.noreply.github.com>
2023-03-31 11:12:45 -0700

    Update main.py

--- Final Project/main.py
+++ Final Project/main.py
@@ -1,33 +1,36 @@
 import js
 p5 = js.window
 
-class Sky1():
+
+class Pepper():
     def __init__(self, x = 0, y = 0):
         self.x = x
         self.y = y
-        self.img1 = p5.loadImage('sky_1.jpg')
+        self.img = p5.loadImage('pepper.jpg')
         
     def draw(self):
         p5.push()  # Save the current transformation state
-        scaleX = p5.width / self.img1.width
-        scaleY = p5.height / self.img1.height
+        scaleX = p5.width / self.img.width
+        scaleY = p5.height / self.img.height
         p5.scale(scaleX, scaleY)  # Scale the image based on the ratio of canvas size to image size
-        p5.image(self.img1, self.x, self.y)  # Draw the image
+        p5.image(self.img, self.x, self.y)  # Draw the image
         p5.pop()  # Restore the transformation state
         
     def draw_dots(self, num_dots = 200, dot_size = 15):
-        scaleX = p5.width / self.img1.width
-        scaleY = p5.height / self.img1.height
+        scaleX = p5.width / self.img.width
+        scaleY = p5.height / self.img.height
     
         for i in range(num_dots):
-            x = p5.random(0, self.img1.width) * scaleX
-            y = p5.random(0, self.img1.height) * scaleY
-            color = self.img1.get(int(x / scaleX), int(y / scaleY))
+            x = p5.random(0, self.img.width) * scaleX
+            y = p5.random(0, self.img.height) * scaleY
+            color = self.img.get(int(x / scaleX), int(y / scaleY))
             p5.fill(color)
             p5.noStroke()
             p5.ellipse(x, y, dot_size, dot_size)
 
-sky1 = Sky1()
+
+pepper = Pepper()
+
 
 def setup():
     p5.createCanvas(300, 300) 
@@ -35,11 +38,12 @@ def setup():
     
 def draw():
     p5.background(255)           
-    sky1.draw()
-    sky1.draw_dots()
+    pepper.draw()
+    pepper.draw_dots()
     
 def keyPressed(event):
     pass 
+
 
 def keyReleased(event):
     pass
@@ -47,6 +51,6 @@ def keyReleased(event):
 def mousePressed(event):
     pass
 
+
 def mouseReleased(event):
     pass
-
