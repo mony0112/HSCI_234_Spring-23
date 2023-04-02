1f60044d3cb3dc18a30427a74fac1d8f8b796b6a
mony0112 <113718634+mony0112@users.noreply.github.com>
2023-04-01 19:08:30 -0700

    Update main.py

--- Final Project/main.py
+++ Final Project/main.py
@@ -1,9 +1,7 @@
 import js
 p5 = js.window
 
-
 arr = []
-
 
 class Pepper():
     def __init__(self, x = 0, y = 0):
@@ -34,13 +32,10 @@ class State1(Pepper):
             # p5.ellipse(x, y, dot_size, dot_size)
             arr.append({'x': x, 'y': y, 'color': color})
 
-
         for item in arr:
-
 
             if p5.mouseX - 50 < item['x'] < p5.mouseX + 50 and p5.mouseY - 50 < item['y'] < p5.mouseY + 50:
                 continue
-
 
             p5.stroke('#FF3FB4')
             p5.strokeWeight(2)
@@ -53,32 +48,83 @@ class State1(Pepper):
             p5.ellipse(item['x'], item['y'], dot_size, dot_size)
             
 class State2(Pepper):
-    def draw_dots(self, num_dots = 10, dot_size = 10):
+    def draw_dots(self, num_dots=10, dot_size=10):
         scaleX = p5.width / self.img.width
         scaleY = p5.height / self.img.height
-    
+        
         for i in range(num_dots):
             x = p5.random(0, self.img.width) * scaleX
             y = p5.random(0, self.img.height) * scaleY
             color = self.img.get(int(x / scaleX), int(y / scaleY))
+            
+            # Change color based on mouse position
+            dist = p5.dist(x, y, p5.mouseX, p5.mouseY)
+            if dist < 20:
+                color = p5.color('#FF3FB4')
+                
             p5.fill(color)
             p5.noStroke()
-            # p5.ellipse(x, y, dot_size, dot_size)
+            arr.append({'x': x, 'y': y, 'color': color})
+        
+        for item in arr:
+            p5.fill(item['color'])
+            p5.noStroke()
+            p5.ellipse(item['x'], item['y'], dot_size, dot_size)
+
+        p5.fill('#FF3FB4')
+        p5.noStroke()
+        p5.ellipse(p5.mouseX, p5.mouseY, 10, 10)
+        
+class State3(Pepper):
+    def draw_dots(self, num_dots=100, dot_size=10):
+        scaleX = p5.width / self.img.width
+        scaleY = p5.height / self.img.height
+
+        for i in range(num_dots):
+            x = p5.random(0, self.img.width) * scaleX
+            y = p5.random(0, self.img.height) * scaleY
+            color = self.img.get(int(x / scaleX), int(y / scaleY))
+
+            # Change size and rotation based on distance from mouse position
+            dist = p5.dist(x, y, p5.mouseX, p5.mouseY)
+            size = p5.map(dist, 0, p5.width, dot_size, 0)
+            angle = p5.map(dist, 0, p5.width, 0, p5.TWO_PI)
+            p5.push()
+            p5.translate(x, y)
+            p5.rotate(angle)
+            p5.noStroke()
+            p5.fill(color)
+            p5.ellipse(0, 0, size, size)
+            p5.pop()
+
+            # Change color based on y position
+            if y < p5.height / 3:
+                color = p5.color('#FF3FB4')
+            elif y < 2 * p5.height / 3:
+                color = p5.color('#4A90E2')
+            else:
+                color = p5.color('#51B46D')
             arr.append({'x': x, 'y': y, 'color': color})
 
-
         for item in arr:
+            if p5.mouseX - 50 < item['x'] < p5.mouseX + 50 and p5.mouseY - 50 < item['y'] < p5.mouseY + 50:
+                continue
+
+            p5.stroke('#FF3FB4')
+            p5.strokeWeight(2)
+            p5.noFill()
+            p5.rect(p5.mouseX - 50, p5.mouseY - 50, 50, 50)
+
+
             p5.fill(item['color'])           
             p5.noStroke()
             p5.ellipse(item['x'], item['y'], dot_size, dot_size)
-            p5.fill('#FF3FB4')
-            p5.ellipse(p5.mouseX, p5.mouseY, 20, 20)
-        
-            
 
+    
 pepper = Pepper()
 pepper1 = State1()
 pepper2 = State2()
+pepper3 = State3()
 
 def setup():
     p5.createCanvas(300, 300) 
@@ -94,9 +140,11 @@ def draw():
             pepper1.draw_dots()
         elif (p5.key == '2'):
             pepper2.draw_dots()
+        elif (p5.key == '3'):
+            pepper3.draw_dots()
     else:
         p5.background(255)
-        
+
     
 def keyPressed(event):
     pass 
